302575c54cb0abe2baa052699a4773698e82beff
Christopher Tracy <93748663+CTrac314159@users.noreply.github.com>
2022-03-14 22:42:17 -0500

    Update tower_fall.cpp

--- tower_fall.cpp
+++ tower_fall.cpp
@@ -4,52 +4,59 @@
 // This program calculates the height of a ball falling from a tower with a user-specified height in
 // meters. The height is calculated every second.
 
-double towerHeight() // Get tower height from the user
+class TowerFall
 {
-    std::cout << "Enter the height of the tower in meters: ";
-    double height{};
-    std::cin >> height;
+private:
+    double ball_height{}; // Private variable for the ball height
 
-    return height;
-}
+public:
 
-double ballHeight(double height, double seconds) // Calculate fall distance at a particular second given 
-                                                 // a tower height and second count
-{
-    // Using formula: [ fall_distance = u_0 * time + (g * time^2) / 2 ], assuming initial velocity (u_0) = 0
-    double fall_distance{ (constants::g * (seconds * seconds)) / 2.0 };
-    double ball_height{ height - fall_distance };
+    double towerHeight() // Get tower height from the user
+    {
+        std::cout << "Enter the height of the tower in meters: ";
+        double height{};
+        std::cin >> height;
 
-    if (ball_height < 0.0) // Check to see if the ball has reached the ground (height of zero)
-        return 0.0;
-    else
-        return ball_height;
-}
+        return height;
+    }
 
-bool calculatePrint(double tower_height, int seconds)
-{
-    double ball_height{ ballHeight(tower_height, static_cast<double>(seconds)) }; // Call the ballHeight function
+    double ballHeight(double height, double seconds) // Calculate fall distance at a particular second given 
+                                                     // a tower height and second count
+    {
+        // Using formula: [ fall_distance = u_0 * time + (g * time^2) / 2 ], assuming initial velocity (u_0) = 0
+        double fall_distance{ (constants::g * (seconds * seconds)) / 2.0 };
+        double ballHeight{ height - fall_distance };
 
-    if (ball_height > 0.0) // If the ball is not on the ground, print the height
-        std::cout << "At " << seconds << " seconds, the height of the ball is " << ball_height << " meters." << '\n';
-    else
-        std::cout << "At " << seconds << " seconds, the ball is on the ground." << '\n';
+        if (ballHeight < 0.0) // Check to see if the ball has reached the ground (height of zero)
+            return 0.0;
+        else
+            return ballHeight;
+    }
 
-    return (ball_height == 0.0); // Return the ball-on-ground Boolean
-}
+    void calculateHeights(double tower_height, int second) // Member function that outputs the ball height at a time, takes tower height
+                                                           // and the second to start at (second 0 = ball at the tower height)
+    {
+        do
+        {
+            ball_height = ballHeight(tower_height, static_cast<double>(second)); // Call the ballHeight member function, assign to ball_height
+            
+            if (ball_height > 0.0) // If the ball is not on the ground, print the height
+                std::cout << "At " << second << " seconds, the height of the ball is " << ball_height << " meters." << '\n';
+            else
+                std::cout << "At " << second << " seconds, the ball is on the ground." << '\n'; // ball_height = 0.0
+
+            ++second;
+        }
+        while (ball_height > 0.0); // Ball-on-ground Boolean
+    }
+};
 
 int main()
 {
-    const double tower_height{ towerHeight() }; // Get input tower height from the user
-    int seconds{ 0 }; // Initialize the time increment
-    bool onGround{ false }; // Initialize Boolean ball-on-ground variable
-
-    do
-    {
-        onGround = calculatePrint(tower_height, seconds++);
-    }
-
-    while (!onGround); // Continue calculating the height until the ball reaches the ground
+    TowerFall tower_fall;
+    const double tower_height{ tower_fall.towerHeight() }; // Get input tower height from the user
+    int second{ 0 }; // Initialize the time increment
+    tower_fall.calculateHeights(tower_height, second);
 
     return 0;
 }
