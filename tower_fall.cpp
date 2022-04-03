34af2717a9b86be6e9c22a925d3e255358c4fe6f
Christopher Tracy <93748663+CTrac314159@users.noreply.github.com>
2022-04-03 13:42:38 -0500

    Update tower_fall.cpp

--- tower_fall.cpp
+++ tower_fall.cpp
@@ -7,25 +7,21 @@
 class TowerFall
 {
 private:
-    double ball_height{}; // Private variable for the ball height
+    double m_ballheight{}; // Private variable for the ball height
+    double m_towerheight{}; // Private variable for the tower height
+    double m_second{}; // Private variable for the time (make seconds a double type for consistency and to avoid overflow)
 
 public:
-
-    double towerHeight() // Get tower height from the user
+    TowerFall(double towerheight = 0.0, int second = 0) : m_towerheight{towerheight}, m_second{static_cast<double>(second)}
     {
-        std::cout << "Enter the height of the tower in meters: ";
-        double height{};
-        std::cin >> height;
-
-        return height;
+        // Constructor to initialize tower height and start time (defaults of 0)
     }
 
-    double ballHeight(double height, double seconds) // Calculate fall distance at a particular second given 
-                                                     // a tower height and second count
+    double findBallHeight() // Calculate fall distance at a particular second given a tower height and second count
     {
         // Using formula: [ fall_distance = u_0 * time + (g * time^2) / 2 ], assuming initial velocity (u_0) = 0
-        double fall_distance{ (constants::g * (seconds * seconds)) / 2.0 };
-        double ballHeight{ height - fall_distance };
+        double fall_distance{ (constants::g * (m_second * m_second)) / 2.0 };
+        double ballHeight{ m_towerheight - fall_distance };
 
         if (ballHeight < 0.0) // Check to see if the ball has reached the ground (height of zero)
             return 0.0;
@@ -33,30 +29,33 @@ public:
             return ballHeight;
     }
 
-    void calculateHeights(double tower_height, int second) // Member function that outputs the ball height at a time, takes tower height
-                                                           // and the second to start at (second 0 = ball at the tower height)
+    void calculateHeights() // Member function that outputs the ball height at a time, takes tower height
+                            // and the second to start at (second 0 = ball at the tower height)
     {
         do
         {
-            ball_height = ballHeight(tower_height, static_cast<double>(second)); // Call the ballHeight member function, assign to ball_height
+            m_ballheight = findBallHeight(); // Call the ballHeight member function, assign to ball_height
             
-            if (ball_height > 0.0) // If the ball is not on the ground, print the height
-                std::cout << "At " << second << " seconds, the height of the ball is " << ball_height << " meters." << '\n';
+            if (m_ballheight > 0.0) // If the ball is not on the ground, print the height
+                std::cout << "At " << m_second << " seconds, the height of the ball is " << m_ballheight << " meters." << '\n';
             else
-                std::cout << "At " << second << " seconds, the ball is on the ground." << '\n'; // ball_height = 0.0
+                std::cout << "At " << m_second << " seconds, the ball is on the ground." << '\n'; // ball_height = 0.0
 
-            ++second;
+            ++m_second;
         }
-        while (ball_height > 0.0); // Ball-on-ground Boolean
+        while (m_ballheight > 0.0); // Ball-on-ground Boolean
     }
 };
 
 int main()
 {
-    TowerFall tower_fall;
-    const double tower_height{ tower_fall.towerHeight() }; // Get input tower height from the user
+    std::cout << "Enter the height of the tower in meters: "; // Get tower height from the user
+    double tower_height{};
+    std::cin >> tower_height;
+    
     int second{ 0 }; // Initialize the time increment
-    tower_fall.calculateHeights(tower_height, second);
+    TowerFall tower_fall{tower_height, second}; // Initialize class object
+    tower_fall.calculateHeights();
 
     return 0;
 }
